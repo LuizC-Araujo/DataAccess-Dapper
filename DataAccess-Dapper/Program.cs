a2d117d19f1d37f0348aaad733c4577c964fdb5b
Luiz Araujo <arauldesign@gmail.com>
2022-07-18 12:01:33 -0300

    Using dapper

--- DataAccess-Dapper/Program.cs
+++ DataAccess-Dapper/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Data.SqlClient;
+
+using Dapper;
+
+using DataAccess_Dapper.Model;
+
 
 namespace DataAccess_Dapper
 {
@@ -6,8 +12,17 @@ namespace DataAccess_Dapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string connectionString = "Server=localhost,1433;Database=meubanco;User ID=user;Password=senha";
 
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id}");
+                }
+            
+            }
         }
     }
 }
