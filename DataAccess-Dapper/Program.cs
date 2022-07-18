0169af455a9c291d7b5a8aa40436570b09fc21c8
Luiz Araujo <arauldesign@gmail.com>
2022-07-18 12:26:46 -0300

    Inserting - sql parameter

--- DataAccess-Dapper/Program.cs
+++ DataAccess-Dapper/Program.cs
@@ -14,12 +14,45 @@ namespace DataAccess_Dapper
         {
             const string connectionString = "Server=localhost,1433;Database=meubanco;User ID=user;Password=senha";
 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destina a serviços da amazon";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id,
+                    @Title,
+                    @Url,
+                    @Summary,
+                    @Order,
+                    @Description,
+                    @Featured)";
+
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    Console.WriteLine($"{category.Id}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+
+                Console.WriteLine($"{rows} linhas inseridas");
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id}");
                 }
             
             }
