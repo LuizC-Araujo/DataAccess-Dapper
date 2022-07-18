6973c3d7235cdd030efc82ba7cc67b24d2f42654
Luiz Araujo <arauldesign@gmail.com>
2022-07-18 14:15:32 -0300

    Query Multiple

--- DataAccess-Dapper/Program.cs
+++ DataAccess-Dapper/Program.cs
@@ -275,6 +275,26 @@ namespace DataAccess_Dapper
             }
         }
 
+        static void QueryMultiple(SqlConnection connection)
+        {
+            var query = "SELECT * FROM [Category]; SELECT * FROM [Course]";
+
+            using(var multi = connection.QueryMultiple(query))
+            {
+                var categories = multi.Read<Category>();
+                var courses = multi.Read<Course>();
+
+                foreach (var item in categories)
+                {
+                    Console.WriteLine(item.Title);
+                }
+
+                foreach (var item in courses)
+                {
+                    Console.WriteLine(item.Title);
+                }
+            }
+        }
     }
 
 
