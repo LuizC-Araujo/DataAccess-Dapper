9fe2a4b46cc6229261ef1d0eea0ffad7c4920792
Luiz Araujo <arauldesign@gmail.com>
2022-07-18 12:33:52 -0300

    Organizando o código

--- DataAccess-Dapper/Program.cs
+++ DataAccess-Dapper/Program.cs
@@ -8,12 +8,33 @@ using DataAccess_Dapper.Model;
 
 namespace DataAccess_Dapper
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=meubanco;User ID=user;Password=senha";
 
+            
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ListCategories(connection);
+                CreateCategory(connection); 
+            }
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id}");
+            }
+
+        }
+
+        static void CreateCategory(SqlConnection connection)
+        {
             var category = new Category();
             category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
@@ -23,39 +44,20 @@ namespace DataAccess_Dapper
             category.Summary = "AWS Cloud";
             category.Featured = false;
 
-            var insertSql = @"INSERT INTO 
-                    [Category] 
-                VALUES(
-                    @Id,
-                    @Title,
-                    @Url,
-                    @Summary,
-                    @Order,
-                    @Description,
-                    @Featured)";
+            var insertSql = @"INSERT INTO [Category] 
+                            VALUES(@Id,@Title,@Url,@Summary,@Order, @Description, @Featured)";
 
-            using (var connection = new SqlConnection(connectionString))
+            var rows = connection.Execute(insertSql, new
             {
-                var rows = connection.Execute(insertSql, new
-                {
-                    category.Id,
-                    category.Title,
-                    category.Url,
-                    category.Summary,
-                    category.Order,
-                    category.Description,
-                    category.Featured
-                });
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
 
-                Console.WriteLine($"{rows} linhas inseridas");
-
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id}");
-                }
-            
-            }
-        }
-    }
+            Console.WriteLine($"{rows} linhas inseridas");
+        }    }
 }
